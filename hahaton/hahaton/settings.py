30b3cf1a1f7a714afb1fe51f9433eba8e5872dd9
Artem Parshyn <parshyn4@gmail.com>
2024-11-28 22:29:05 +0300

    initial commit

--- hahaton/hahaton/settings.py
+++ hahaton/hahaton/settings.py
@@ -55,7 +55,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
