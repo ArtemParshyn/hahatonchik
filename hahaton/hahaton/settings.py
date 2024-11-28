64a6e7dc2e37e63c80add7e87ea1c71652d57391
Artem Parshyn <parshyn4@gmail.com>
2024-11-28 22:23:33 +0300

    initial commit

--- hahaton/hahaton/settings.py
+++ hahaton/hahaton/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -55,8 +54,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -68,7 +65,6 @@ MIDDLEWARE = [
 ]
 
 STATIC_URL = 'static/'
-
 
 ROOT_URLCONF = 'hahaton.urls'
 
@@ -90,7 +86,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hahaton.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -100,7 +95,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -120,7 +114,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -131,7 +124,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
