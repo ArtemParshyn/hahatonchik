23a4a2e3a19048b7eeebe67d33838157e0d0cab8
Aichepsh <eduard_ai@mail.ru>
2024-11-30 13:12:39 +0300

    Главная страница 2 коммит

--- hahaton/api/urls.py
+++ hahaton/api/urls.py
@@ -4,4 +4,7 @@ from api import views
 
 urlpatterns = [
         path('', views.index, name="index"),
+        path('services', views.services, name="index"),
+        path('about', views.about, name="index"),
+        path('contact', views.contact, name="index"),
 ]
--- hahaton/api/views.py
+++ hahaton/api/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
