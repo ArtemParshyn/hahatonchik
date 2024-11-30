3fefbc705733e6f1eaa700931f6e6d9281b992a0
Kweyzzz <amal.5555@mail.ru>
2024-11-30 13:26:38 +0300

    index

--- hahaton/api/views.py
+++ hahaton/api/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
-
-# Create your views here.
+# Главная страница
 def index(request):
     return render(request, 'index.html')
+
+# Страница поиска сотрудников
+def services(request):
+    return render(request, 'services.html')
+
+# Страница со списком всех сотрудников
+def all_employees(request):
+    return render(request, 'all_employees.html')
+
+# Страница "О нас"
+def about(request):
+    return render(request, 'about.html')
